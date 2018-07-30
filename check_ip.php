82b178fd8bd163d42ba375c126e7ae3329c3558c
lorddown <41264250+lorddown@users.noreply.github.com>
2018-07-30 12:17:34 -0300

    Update check_ip.php

--- check_ip.php
+++ check_ip.php
@@ -7,29 +7,27 @@ error_reporting(0);
 
 // stdin loop
 if (!defined(STDIN)) {
-	define("STDIN", fopen("php://stdin", "r"));
+        define("STDIN", fopen("php://stdin", "r"));
 }
 if (!defined(STDOUT)) {
-	define("STDOUT", fopen('php://stdout', 'w'));
+        define("STDOUT", fopen('php://stdout', 'w'));
 }
 while (!feof(STDIN)) {
 
-	$check_ip = trim(fgets(STDIN));
+        $check_ip = trim(fgets(STDIN));
 
-	$status = squid_check_ip($db, $check_ip);
+        $status = squid_check_ip($db, $check_ip);
 
-	if (isset($status)) {
-		fwrite(STDOUT, "OK user={$status}\n");
-	} else {
-		fwrite(STDOUT, "ERR\n");
-	}
+        if (isset($status)) {
+                fwrite(STDOUT, "OK user={$status}\n");
+        } else {
+                fwrite(STDOUT, "ERR");
+        }
 }
 function squid_check_ip($db, $check_ip) {
-	exec("sqlite3 -init 1000 {$db} \"SELECT ip FROM captiveportal WHERE ip='{$check_ip}'\"", $ip);
-	if ($check_ip == $ip[0]) {
-		exec("sqlite3 -init 1000 {$db} \"SELECT username FROM captiveportal WHERE ip='{$check_ip}'\"", $user);
-		return $user[0];
-	}
+        exec("sqlite3 -init 10000 {$db} \"SELECT ip FROM captiveportal WHERE ip='{$check_ip}'\"", $ip);
+        if ($check_ip == $ip[0]) {
+                exec("sqlite3 -init 10000 {$db} \"SELECT username FROM captiveportal WHERE ip='{$check_ip}'\"", $user);
+                return $user[0];
+        }
 }
-
-?>
